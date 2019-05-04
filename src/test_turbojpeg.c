caa376e383cd3177ebceadd20357637bce4ba234
Yang, Zhijie <yangzhj@shanghaitech.edu.cn>
2019-05-04 22:29:47 +0800

    Made turbojpeg benchmark to work

--- src/test_turbojpeg.c
+++ src/test_turbojpeg.c
@@ -6,12 +6,12 @@
  
 // #include "jpeg-utils.h"
 // #include "libjpeg/include/turbojpeg.h"
-#include  <dirent.h>
+#include <dirent.h>
 #include <unistd.h>
 
 #include <turbojpeg.h>
 
-#define NUM_IMG 100
+#define NUM_IMG 900
 
 
 struct JpegHeader
@@ -115,34 +115,55 @@ void read_dir_img (char path[])
     }
 
     int i = -1;
+
+    // if ((dir=opendir("pics")) == NULL)
+    // {
+    //     perror("Open dir \"pic/\" encountered an error...\n");
+    //     exit(1);
+    // }
+
     while ((ptr=readdir(dir)) != NULL, i < NUM_IMG)
     {
+        // printf ("i = %d\n", i);
         i += 1;
         if(strcmp(ptr->d_name,".")==0 || strcmp(ptr->d_name,"..")==0)    ///current dir OR parrent dir
             continue;
         else if(ptr->d_type == 8)    ///file
         {
+            // printf ("It is a regular file. File name: %s\n", ptr->d_name);
             FILE *imgFile = NULL;
             unsigned char *jpegBuf = NULL;
             int size = 0;
+            imgFile = fopen(ptr->d_name, "rb");
+            if (imgFile == NULL)
+            {
+                printf ("Unable to open such file.\n");
+                exit (1);
+            }
             if (fseek(imgFile, 0, SEEK_END) < 0 || ((size = ftell(imgFile)) < 0) ||
             fseek(imgFile, 0, SEEK_SET) < 0)
             {
-                // printf ("File size error.\n");
+                printf ("File size error.\n");
+                exit (1);
             }
             if (size == 0)
             {
-                // printf ("Size equals to zero");
+                printf ("Size equals to zero");
+                exit (1);
             }
             unsigned long jpegSize = (unsigned long)size;
             // unsigned char *jpegBuf = NULL;
             if ((jpegBuf = (unsigned char *)tjAlloc(jpegSize)) == NULL)
             {
                 // THROW_UNIX("allocating JPEG buffer");
+                printf ("Allocation for jpeg buffer failed");
+                exit (1);
             }
             if (fread(jpegBuf, jpegSize, 1, imgFile) < 1)
             {
                 // THROW_UNIX("reading input file");
+                printf ("Unable to read this file to the buffer");
+                exit (1);
             }
             fclose(imgFile);  imgFile = NULL;
             tjpeg2rgb(jpegBuf, jpegSize, imgArray[i], i);
@@ -150,7 +171,7 @@ void read_dir_img (char path[])
         }
         else 
         {
-        continue;
+            continue;
         }
     }
 
@@ -165,9 +186,10 @@ long int compress_img_array ()
     for (int i = 0; i < NUM_IMG; i += 1)
     {
         trgb2jpeg(imgArray[i], imgHeaderArray[i].width, imgHeaderArray[i].height, 75, &jpegBuf, &(imgHeaderArray[i].size));
-        free (jpegBuf);
+        // free (jpegBuf);
         totalSize += imgHeaderArray[i].size;
     }
+    // free (jpegBuf);
     return totalSize;
 }
 
@@ -176,15 +198,32 @@ long int compress_img_array ()
 int main()
 {
     char cwd[128];
-    getcwd(cwd,sizeof(cwd));
-    read_dir_img (cwd);
-    clock_t start = clock ();
-    for (int j = 0; j < 5; j++)
+    long int totalSize = 0;
+    int ch_res = chdir ("pics");
+    if (ch_res == -1)
     {
-        long int totalSize = compress_img_array ();
+        printf ("Unable change directory to \"pic\".\n");
+        exit(1);
     }
-    clock_t end = clock ();
-    double duration = (end - start) / 5;
-    printf ("Average duration for compressing %d images is %lf miliseconds.\n", NUM_IMG, duration);
+    getcwd(cwd, sizeof(cwd));
+    printf ("Current directory: %s\n", cwd);
+    read_dir_img (cwd);
+    // clock_t start = clock ();
+    struct timespec start, end;
+    clock_gettime (CLOCK_MONOTONIC_RAW, &start);
+    printf ("compressing %d images.\n", NUM_IMG);
+    for (int j = 0; j < 100; j++)
+    {
+        totalSize = compress_img_array ();
+    }
+    // clock_t end = clock ();
+    clock_gettime(CLOCK_MONOTONIC_RAW, &end);
+    time_t _duration = end.tv_nsec - start.tv_nsec;
+    printf ("Start time: %ld\n", start.tv_nsec);
+    printf ("End time: %ld\n", end.tv_nsec);
+    printf ("Total duration: %ld nanoseconds.\n", _duration);
+    long double duration = _duration / 100 / 1000;
+    printf ("Average duration for compressing %ld bytes of image is %Lf microseconds.\n", totalSize, duration);
+    printf ("Average compression speed is %Lf MBps.\n", totalSize / duration / 1000);
 
 }
