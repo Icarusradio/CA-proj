aad21f7476e3c9903b3939e2130d98f7aa70449d
Icarus Radio <Icarusradio@users.noreply.github.com>
2019-05-07 11:45:26 +0800

    Update client.c

--- src/client.c
+++ src/client.c
@@ -8,22 +8,24 @@
 #include <sys/types.h>
 #include <unistd.h>
 
-#define MAX_LEN 1024
+// Maximum file size (1 MiB)
+#define MAXFILE 1048576
+
+#define BUFSIZE 8192
 
 static void
 usage ()
 {
   printf ("Usage:\n");
-  printf ("server FILENAME PORT SERVER_IP\n");
-  printf ("FILENAME: file to send or receive.\n");
-  printf ("PORT: port to accept or send file.\n");
+  printf ("server SERVER_IP PORT\n");
   printf ("SERVER_IP: the server's IP.\n");
+  printf ("PORT: port to accept or send file.\n");
 }
 
 int
 main (int agrc, const char **argv)
 {
-  if (agrc != 4)
+  if (agrc != 3)
     {
       usage ();
       return 1;
@@ -36,7 +38,7 @@ main (int agrc, const char **argv)
   hints.ai_family = AF_UNSPEC; /* don't care IPv4 or IPv6 */
   hints.ai_socktype = SOCK_STREAM; /* TCP stream sockets */
   
-  status = getaddrinfo (argv[3], argv[2], &hints, &res);
+  status = getaddrinfo (argv[1], argv[2], &hints, &res);
   if (status != 0)
     goto error;
 
@@ -47,32 +49,40 @@ main (int agrc, const char **argv)
     goto error;
 
   // connect!
-  status = connect(sockfd, res->ai_addr, res->ai_addrlen);
+  status = connect (sockfd, res->ai_addr, res->ai_addrlen);
   if (status != 0)
     goto error;
 
-  int i = 10;
-  send (sockfd, &i, sizeof i, MSG_EOR);
+  // Notify the server
+  status = 10;
+  send (sockfd, &status, sizeof status, MSG_EOR);
 
-  char buf[MAX_LEN];
-  mode_t mode = S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH;
-  int filefd = open (argv[1], O_WRONLY | O_CREAT | O_TRUNC, mode);
-  if (filefd == -1)
-    goto error;
-
-  ssize_t bytes = recv (sockfd, buf, MAX_LEN, MSG_EOR);
+  char buf[MAXFILE];
+  ssize_t bytes = recv (sockfd, buf, BUFSIZE, MSG_EOR);
+  off_t offset = bytes;
   while (bytes != 0)
     {
-      write (filefd, buf, bytes);
-      bytes = recv (sockfd, buf, MAX_LEN, MSG_EOR);
+      bytes = recv (sockfd, buf + offset, BUFSIZE, MSG_EOR);
+      if (bytes != BUFSIZE)
+        {
+          offset = 0;
+
+          // Compress the image
+        }
+      else
+        offset += bytes;
     }
 
-  close (filefd);
+  // Tell the server that we are done
+  status = 10;
+  send (sockfd, &status, sizeof status, MSG_EOR);
+
   close (sockfd);
+  freeaddrinfo (res);
 
   return 0;
 
  error:
-  fprintf(stderr, "Error!\n");
+  fprintf(stderr, "Error! %d\n", errno);
   return 1;
 }
